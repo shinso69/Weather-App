741d7362e86df94a6c7f2e3aed13a7aadb11f211
Armando Santos <adavid988@gmail.com>
2020-04-05 16:58:03 +0100

    Remove unnecessary code

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-fetch("http://localhost:3000/weather?address=!").then(res => {
-  res.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
-  });
-});
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
